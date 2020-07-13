9273a8af324f622be2185cec1b10de5d28f9865e
papadopan <antonios.papadopan@gmail.com>
2020-07-13 17:19:17 +0200

    ui: update 404,403
    Signed-off-by: papadopan <antonios.papadopan@gmail.com>

--- ui/cap-react/src/components/errors/404.js
+++ ui/cap-react/src/components/errors/404.js
@@ -6,19 +6,8 @@ import FormPreviousLinkIcon from "grommet/components/icons/base/FormPreviousLink
 
 const NotFoundPage = () => {
   return (
-    <Box flex>
-      <Box align="center" full="horizontal" pad="medium" />
-      <Box flex colorIndex="light-2" align="center">
-        <Label>404 Page Not Found</Label>
-      </Box>
-      <Box>
-        <Anchor
-          icon={<FormPreviousLinkIcon />}
-          label="Go back to dashboard"
-          path="/"
-          size="small"
-        />
-      </Box>
+    <Box flex colorIndex="light-1">
+      <div>as</div>
     </Box>
   );
 };
